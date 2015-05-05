e1cfe85d69e7ba3a1362028d41720d8d89aed5c7
Richard Saldivar <rsaldiva@gmail.com>
2015-05-04 22:56:33 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TheOdinProject-Javascript-Query-Project
+# etch-a-sketch
+Etch-a-sketch javascript/jquery project for <a href="http://www.theodinproject.com/web-development-101/javascript-and-jquery?ref=lc-pb">The Odin Project</a>.
+
+See demo <a href="">here</a>.
---
+++ js/custom.js
@@ -0,0 +1,87 @@
+var number = 16;
+var color = "#660066";
+$(document).ready(function(){
+	initialCanvas();
+	/*var table = document.getElementById("canvas");
+    for(i = 0; i <= number; i++)
+	{
+		var row = table.insertRow(i);
+		for(j = 0; j <= number; j++)
+		{
+			row.insertCell(j);
+		}
+	};
+	$('table tbody td').mouseover(function() {
+		$(this).addClass('selectedCell');
+	});*/
+	 $('#default').click(initialCanvas); 
+	$('#resize').click(resizeCanvas);
+	$('#color').click(createColor);
+	
+	function initialCanvas(){
+		$( "#canvas" ).empty();
+		var table = document.getElementById("canvas");
+		number = 16;
+		color = "#660066";
+		for(i = 0; i <= number; i++)
+		{
+			var row = table.insertRow(i);
+			for(j = 0; j <= number; j++)
+			{
+			row.insertCell(j);
+			}
+	};
+	$('table tbody td').mouseover(function() {
+		$(".selectedCell").css("backgroundColor", color);
+		$(this).addClass('selectedCell');
+	});}
+	
+	function resizeCanvas() {
+		$( "#canvas" ).empty();
+		number = prompt("How 'pixels' would you like on each side? (1-100)");
+		var table = document.getElementById("canvas");
+		for(i = 0; i <= number; i++)
+		{
+			var row = table.insertRow(i);
+			for(j = 0; j <= number; j++)
+			{
+				row.insertCell(j);
+			}
+		};
+		$('table tbody td').mouseover(function() {
+			$(".selectedCell").css("backgroundColor", color);
+			 $(this).addClass('selectedCell');
+		  })/*.mouseout(function() {
+			 $(this).removeClass('selectedCell');
+		  }).click(function() {
+			 alert($('td:first', this).text());
+		  });*/
+		};
+	
+	function createColor() {
+		$( "#canvas" ).empty();
+		color = prompt("What color would you like to use?")
+			var table = document.getElementById("canvas");
+		$(".selectedCell").css("backgroundColor", color);
+		for(i = 0; i <= number; i++)
+		{
+			var row = table.insertRow(i);
+			for(j = 0; j <= number; j++)
+			{
+				row.insertCell(j);
+			}
+		};
+		$('table tbody td').mouseover(function() {
+			$(".selectedCell").css("backgroundColor", color);
+			 $(this).addClass('selectedCell');
+		  })/*.mouseout(function() {
+			 $(this).removeClass('selectedCell');
+		  }).click(function() {
+			 alert($('td:first', this).text());
+		  });*/
+		};
+    
+});
+
+
+
